b58539d9147372bc564b94a72d0d188ea5a82ec2
Filippo Di Costanzo <filippodicostanzo@gmail.com>
2021-09-15 12:35:39 +0200

    Added Artists Pages

---
+++ pages/artists/_id/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    {{ artist.name }}
+  </div>
+</template>
+
+<script>
+import getData from "@/mixins/fetchData";
+
+export default {
+  name: "index.vue",
+  data() {
+    return {
+      artists: [],
+      artist: {
+        name: '',
+        idenfifier: '',
+      },
+    }
+  },
+  mounted() {
+    getData.getArtists().then((result) => {
+      this.artists = result;
+      console.log(this.artists);
+      const id = this.$route.params.id;
+      this.artist = this.artists.find(x => x.idenfifier === id);
+      console.log(this.artist);
+    });
+
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/artists/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Carousel :data="artists" source="artists"/>
+  </div>
+</template>
+
+<script>
+import getData from "@/mixins/fetchData";
+
+export default {
+  name: "index.vue",
+  data() {
+    return {
+      artists: []
+    }
+  },
+
+  mounted() {
+    getData.getArtists().then((result) => {
+      this.artists = result;
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
