1b76bd49caa0e826e664895c50e89aab7096a743
filippodicostanzo <filippodicostanzo@gmail.com>
2023-12-05 12:18:20 +0100

    Added Privacy Policy App Page

---
+++ pages/privacy.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+  name: "privacy"
+}
+</script>
+
+<template>
+  <div class="privacy-policy container py-5">
+    <h1>Privacy Policy for the Wedding Deejay App</h1>
+
+    <h2>Introduction</h2>
+    <p>At Wedding Deejay, we are committed to maintaining the trust and confidence of our app users. This Privacy Policy
+      provides detailed information on when and why we collect your personal information, how we use it, the limited
+      conditions under which we may disclose it to others, and how we keep it secure.</p>
+
+    <h2>Information We Collect</h2>
+    <ul>
+      <li><strong>Personal Identification Information:</strong> We collect and use your name and surname to create a
+        personalized user experience and for user authentication.
+      </li>
+      <li><strong>Email Address:</strong> Your email address is essential for account setup, verification, and for
+        communication purposes. It enables us to send important app updates, security information, and assist with
+        customer service inquiries.
+      </li>
+      <li><strong>Physical Address:</strong> We gather your physical address to offer location-specific services and
+        functionalities. This information plays a crucial role in fraud prevention and customizing your app experience.
+      </li>
+    </ul>
+
+    <h2>Why We Collect Data</h2>
+    <p>Authentication and User Experience: To ensure each user account is securely linked to the correct individual,
+      enhancing your app experience. Operational Efficiency: Your information aids in keeping our servers operational,
+      reducing app crashes, and improving overall performance and scalability. Security and Fraud Prevention: We utilize
+      your data to strengthen our security measures, protecting your information and our platform from unauthorized
+      access and fraudulent activities. Customer Support: To provide you with timely and effective assistance regarding
+      any app-related queries or issues.</p>
+
+    <h2>Data Usage</h2>
+    <p>Improving Services: We analyze collected data to enhance app functionalities and user experience. Communication:
+      We use your contact information to communicate important app changes, promotional offers, or surveys to improve
+      our services. Legal Compliance: Your information may be used to comply with applicable laws and regulations.</p>
+
+    <h2>Data Protection</h2>
+    <p>We implement a variety of security measures to maintain the safety of your personal information. These measures
+      include advanced encryption, secure server storage, and strict data access controls. Our dedicated team regularly
+      reviews and updates our security practices to ensure the highest level of protection.</p>
+
+    <h2>Data Sharing and Disclosure</h2>
+    <p>Third-Party Services: We may share your information with trusted third-party service providers only to the extent
+      necessary for them to perform services on our behalf. Legal Requirements: If required by law, we may disclose your
+      information in response to legal processes or when it’s necessary for the protection of our legal rights. Business
+      Transfers: In the event of a merger, acquisition, or sale of assets, your information may be transferred as part
+      of the transaction.</p>
+
+    <h2>Your Rights</h2>
+    <p>Access and Control: You have the right to access, update, or delete your personal information at any time.
+      Opt-Out: You can opt-out of receiving marketing communications from us by following the unsubscribe link in our
+      emails.</p>
+
+    <h2>Contact Information</h2>
+    <p>For any questions or concerns regarding this privacy policy, please contact us at [insert contact
+      information].</p>
+
+    <h2>Updates to Our Privacy Policy</h2>
+    <p>We may update this policy from time to time. All updates will be posted on this page, and if the changes are
+      significant, we will provide a more prominent notice.</p>
+  </div>
+
+</template>
+
+<style scoped>
+.privacy-policy {
+  font-family: Arial, sans-serif;
+}
+
+.privacy-policy h1, .privacy-policy h2 {
+  color: #333;
+}
+
+.privacy-policy p, .privacy-policy ul {
+  color: #666;
+  line-height: 1.6;
+}
+</style>
