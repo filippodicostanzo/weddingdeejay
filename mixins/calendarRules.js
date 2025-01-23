c6dd0fb366960989b3ace4686a50b16e93a6c26d
filippodicostanzo <filippodicostanzo@gmail.com>
2025-01-23 01:21:08 +0100

    Remove Legacy Files After Nuxt 3 Migration

--- mixins/calendarRules.js
+++
@@ -1,7 +0,0 @@
-export default {
-  methods: {
-    notBeforeToday(date) {
-      return date < new Date(new Date().setHours(0, 0, 0, 0));
-    }
-  }
-}
--- plugins/truncate.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-export default () => {
-  Vue.filter('truncate', function (string, value) {
-    return string.substring(0, value) + '…';
-  })
-}
